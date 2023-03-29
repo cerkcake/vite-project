589ec6701922556fd2cb23e7180ee9450d443fe3
kunlanis <yoo.kunlanis@gmail.com>
2023-03-29 12:27:37 +0700

    fetch API MoviesList

--- vite-project/src/components/Nav.jsx
+++ vite-project/src/components/Nav.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import InputBase from "@mui/material/InputBase";
 import PropTypes from "prop-types";
 import useScrollTrigger from "@mui/material/useScrollTrigger";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -13,7 +11,7 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import SearchIcon from "@mui/icons-material/Search";
 // import { useNavigate } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Search,SearchIconWrapper,StyledInputBase } from "./style";
 
 function ElevationScroll(props) {
   const { children, window } = props;
@@ -40,47 +38,7 @@ ElevationScroll.propTypes = {
   window: PropTypes.func,
 };
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
 
 const Nav = (props) => {
   // const navigate = useNavigate();
@@ -140,7 +98,7 @@ const Nav = (props) => {
               component="div"
               sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
             >
-              Movies Mania
+              Movies888
             </Typography>
             <Search>
               <SearchIconWrapper>
--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -3,15 +3,31 @@ import axios from "axios";
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
-  useEffect(() => {axios({
-    method: "get",
-    url: "https://api.themoviedb.org/3/trending/all/week?api_key=229a6a0f891df5bf1176a4668af885c6",
-  }).then((response) => {
-    console.log("Res.Data : ", response.data);
-    setMovies(response.data);
-  });}, []);
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "https://api.themoviedb.org/3/trending/all/week?api_key=229a6a0f891df5bf1176a4668af885c6",
+    }).then((response) => {
+      console.log("Res.Data : ", response.data.results);
+      setMovies(response.data.results);
+    });
+  }, []);
 
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <ul>
+      {movies.map((m) => {
+        return (
+          <li>
+            {m.title
+              ? m.title
+              : m.original_title
+              ? m.original_title
+              : m.original_name}
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default Home;
